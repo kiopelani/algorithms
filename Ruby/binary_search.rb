b3fbce7d7d420189da9818432b80b4f4695f7bb4
Kiope Gyzen <kiopelani@gmail.com>
2015-01-06 12:47:30 -0800

    initial binary_search method

--- Ruby/binary_search.rb
+++ Ruby/binary_search.rb
@@ -1,19 +1,37 @@
 require_relative 'test_methods'
-# Binary Search
 
-def binary_search(array, item_to_find)
+def binary_search(array, item)
+  upper = array.length - 1
+  lower = 0
+  while upper >= lower
+    mid = (upper + lower) / 2
+    if array[mid] < item
+      lower = mid + 1
+    elsif array[mid] > item
+      upper = mid - 1
+    else
+      return mid
+      break
+    end
+  end
 end
 
 # Tests
-
 sorted_array = [1,2,3,4,5]
-sorted_array_dups = [2,2,5,6,6,20]
+sorted_array_even = [2,4,6,8,10,12]
 
-puts "#binary_search should return the correct index of an item"
+puts "#binary_search should return the correct index of an item for an odd length array"
 
 assert_equals(2, binary_search(sorted_array, 3))
 assert_equals(0, binary_search(sorted_array, 1))
 assert_equals(3, binary_search(sorted_array, 4))
-assert_equals(nil, binary_search(sorted_array, 27))
+assert_equals(nil, binary_search(sorted_array, 0))
+
+puts ""
+puts "#binary_search should return the correct index of an item for an even length array"
+assert_equals(3, binary_search(sorted_array_even, 8))
+assert_equals(0, binary_search(sorted_array_even, 2))
+assert_equals(4, binary_search(sorted_array_even, 10))
+assert_equals(nil, binary_search(sorted_array_even, 100))
 
 
--- Ruby/test_methods.rb
+++ Ruby/test_methods.rb
@@ -2,6 +2,5 @@ def assert_equals(expected, actual)
     puts "*" * 50
     p expected == actual
     puts "expected #{expected} and got #{actual}"
-    puts "*" * 50
 end
 
