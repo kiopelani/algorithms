d0d14801aa0b6a88d27c79118ff2f73b3cbfaff7
Kiope Gyzen <kiopelani@gmail.com>
2015-01-13 10:29:02 -0800

    basic ruby stack implementation

---
+++ Data_Structures/Ruby/stack.rb
@@ -0,0 +1,44 @@
+#Implement a basic stack in Ruby
+
+class Stack
+  def initialize(max_size)
+    @store = []
+    @max_size = max_size
+  end
+
+  def push(element)
+    unless full?
+      @store.push(element)
+      return @store
+    end
+    nil
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def size
+    @store.length
+  end
+
+  private
+  def full?
+    size >= @max_size ? true : false
+  end
+end
+# TESTS
+
+my_stack = Stack.new(3)
+
+p my_stack.size == 0
+p my_stack.pop == nil
+p my_stack.push(1) == [1]
+p my_stack.push(2) == [1,2]
+p my_stack.push(3) == [1,2,3]
+p my_stack.push(4) == nil
+p my_stack.size == 3
+p my_stack.pop == 3
+p my_stack.pop == 2
+p my_stack.size == 1
+
